975187f32eada5c875c93928b15261dd450610e5
mdclement <mike@softwareontheside.com>
2019-08-10 15:54:18 -0400

    added tortoisemerge diff reporting

--- test/rental_test.py
+++ test/rental_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from approvaltests.approvals import verify
-from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
+from approvaltests.reporters.generic_diff_reporter import GenericDiffReporter
 
 from src.customer import Customer
 from src.movierental import MovieRental
@@ -10,7 +10,7 @@ from src.rentalinfo import RentalInfo
 
 class Tests(unittest.TestCase):
     def setUp(self):
-        self.reporter = GenericDiffReporterFactory().get("TortoiseMerge")
+        self.reporter = GenericDiffReporter(("Tortoise", "C:/Program Files/TortoiseSVN/bin/tortoisemerge.exe"))
 
     def test_it_should_return_the_proper_result(self):
         customer = Customer("martin", [MovieRental("F001", 3), MovieRental("F002", 1)])
